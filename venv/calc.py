b8f077337ed301eb3cf1adaf23cbc65e85e5266e
ghazyayman <muhghazyayman@gmail.com>
2022-09-24 14:08:32 +0800

    my pycalc

---
+++ venv/calc.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+class Calculator:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("375x667")
+        self.window.resizable(0,0)
+        self.window.title("Calculator")
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()

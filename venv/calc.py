c76566bf1323fb2de404458d80dde124b16c60b7
ghazyayman <muhghazyayman@gmail.com>
2022-09-24 14:16:14 +0800

    add frame

--- venv/calc.py
+++ venv/calc.py
@@ -1,11 +1,27 @@
 import tkinter as tk
 
+LIGHT_GRAY = "#F5F5F5"
 class Calculator:
     def __init__(self):
         self.window = tk.Tk()
         self.window.geometry("375x667")
         self.window.resizable(0,0)
         self.window.title("Calculator")
+
+
+        self.total_expression = "0"
+        self.current_expression = "0"
+        self.display_frame = self.create_display_frame()
+        self.buttons_frame = self.create_buttons_frame()
+    def create_display_frame(self):
+        frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill="both")
+        return frame
+
+    def create_buttons_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expand=True, fill="both")
+        return frame
 
     def run(self):
         self.window.mainloop()
